f64881fd3a389b49eb94e02d9552f48949187729
Esha Datta <edatta@crossref.org>
2021-10-25 12:14:28 -0400

    getting update and injection to work

--- vue2-vuetify/src/controls/GeoNamesLookupRenderer.vue
+++ vue2-vuetify/src/controls/GeoNamesLookupRenderer.vue
@@ -30,8 +30,10 @@
 <script lang="ts">
 import {
   ControlElement,
+  JsonFormsSubStates,
   Dispatch,
   Actions,
+  JsonSchema,
   JsonFormsRendererRegistryEntry,
   rankWith,
   isIntegerControl,
@@ -49,6 +51,7 @@ import { default as ControlWrapper } from './ControlWrapper.vue';
 import { useVuetifyControl } from '../util';
 import { VTextField } from 'vuetify/lib';
 import { CoreActions } from '@jsonforms/core';
+import set from 'lodash/fp/set';
 
 const controlRenderer = defineComponent({
   name: 'geonames-lookup-renderer',
@@ -61,11 +64,14 @@ const controlRenderer = defineComponent({
   },
   setup(props: RendererProps<ControlElement>) {
     const dispatch = inject<Dispatch<CoreActions>>('dispatch');
+    const jsonforms = inject<JsonFormsSubStates>('jsonforms');
+    const s = jsonforms?.core?.schema;
+    const ui = jsonforms?.core?.uischema;
     const vControl = useVuetifyControl(
       useJsonFormsControl(props),
       (value) => parseInt(value, 10) || undefined
     );
-    return { ...vControl, dispatch };
+    return { ...vControl, dispatch, jsonforms, s, ui };
   },
   methods: {
     mapDict() {
@@ -82,6 +88,21 @@ const controlRenderer = defineComponent({
           geoname: 'name',
           type: 'string',
         },
+        {
+          path: 'addresses.city',
+          geoname: 'name',
+          type: 'string',
+        },
+        {
+          path: 'country.country_name',
+          geoname: 'countryName',
+          type: 'string',
+        },
+        {
+          path: 'country.country_code',
+          geoname: 'countryCode',
+          type: 'string',
+        },
       ];
     },
     checkData(data: string, type: string) {
@@ -94,29 +115,80 @@ const controlRenderer = defineComponent({
           return data;
       }
     },
+    processGeoNamesAdmin(level: string) {
+      return [
+        { ror: 'name', geoname: 'adminName' + level, type: 'string' },
+        { ror: 'ascii_name', geoname: 'adminName' + level, type: 'string' },
+        { ror: 'id', geoname: 'adminId' + level, type: 'int' },
+      ];
+    },
     fetchAddress(id: string) {
       const url = new URL('http://api.geonames.org/getJSON');
       const params = { geonameId: id, username: 'roradmin' }; // or:
       url.search = new URLSearchParams(params).toString();
+      const rootData = this.jsonforms?.core?.data;
       fetch(url.toString()).then((response) => {
         response.json().then((data) => {
           if (this.dispatch) {
             let mapping = this.mapDict();
+            let updatedData = rootData;
+            updatedData = set(
+              'addresses.geonames_city.id',
+              parseInt(id),
+              updatedData
+            );
             for (const entry of mapping) {
-              this.dispatch(
-                Actions.update(entry.path, () =>
-                  this.checkData(data[entry.geoname], entry.type)
-                )
+              updatedData = set(
+                entry.path,
+                this.checkData(data[entry.geoname], entry.type),
+                updatedData
               );
             }
+            if (data.adminId1) {
+              let path = 'addresses.geonames_city.geonames_admin1.';
+              let geonamesAdmin1 = this.processGeoNamesAdmin('1');
+              for (const admin of geonamesAdmin1) {
+                updatedData = set(
+                  path + admin.ror,
+                  this.checkData(data[admin.geoname], admin.type),
+                  updatedData
+                );
+              }
+              updatedData = set(
+                path + 'code',
+                [data.countryCode, data.adminCode1].join('.'),
+                updatedData
+              );
+              if (data.adminId2) {
+                let path = 'addresses.geonames_city.geonames_admin2.';
+                let geonamesAdmin2 = this.processGeoNamesAdmin('2');
+                for (const admin of geonamesAdmin2) {
+                  updatedData = set(
+                    path + admin.ror,
+                    this.checkData(data[admin.geoname], admin.type),
+                    updatedData
+                  );
+                }
+                updatedData = set(
+                  path + 'code',
+                  [data.countryCode, data.adminCode1, data.adminCode2].join(
+                    '.'
+                  ),
+                  updatedData
+                );
+              }
+            }
+            this.dispatch(
+              Actions.updateCore(updatedData, this.s as JsonSchema, this.ui)
+            );
           }
         });
       });
     },
     onChange(e: number) {
-      console.log(e);
       const id = e.toString();
       this.fetchAddress(id);
+      this.control.data = e;
     },
   },
   computed: {
